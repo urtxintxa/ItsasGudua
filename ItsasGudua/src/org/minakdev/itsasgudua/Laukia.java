c4e527d85fad9cc3c45b75a5719d502434b18c41
jajasuperman <enekosampedro@gmail.com>
2015-04-16 20:23:48 +0200

    Hiru

---
+++ ItsasGudua/src/org/minakdev/itsasgudua/Laukia.java
@@ -0,0 +1,69 @@
+package org.minakdev.itsasgudua;
+
+public class Laukia {
+	
+	private boolean begiratuta = false;
+	private Itsasontzia itsasontzia = false;
+	private boolean mina = false;
+	
+	public Laukia() {}
+	
+	public void nireaInprimatu() {
+		if(this.mina) {
+			if(this.begiratuta) {
+				System.out.print("☠"); //mina boooom
+			}
+			else {
+				System.out.println("☣"); //mina 
+			}
+		}
+		else if(this.itsasontzia != null) {
+			System.out.print("⏅");
+		}
+		else {
+			System.out.print("~"); // ura
+		}
+	}
+	
+	public void aurkariarenaInprimatu() {
+		if(!this.begiratuta) {
+			System.out.print("·");
+		}
+		else {
+			if(this.mina) {
+				System.out.print("☠"); //mina boooom
+			}
+			else if(this.itsasontzia != null) {
+				System.out.print("✝"); //itsasontzi hondoratua
+			}
+			else {
+				System.out.print("~"); //ura
+			}
+		}
+	}
+	
+	public boolean getBegiratuta() {
+		return this.begiratuta;
+	}
+	
+	public Itsasontzia getItsasontzia() {
+		return this.itsasontzia;
+	}
+	
+	public boolean getMina() {
+		return this.mina;
+	}
+	
+	public void setBegiratuta(boolean pBegiratuta) {
+		this.begiratuta = pBegiratuta;
+	}
+	
+	public void setItsasontzia(Itsasontzia pItsasontzia) {
+		this.itsasontzia = pItsasontzia;
+	}
+	
+	public void setMina(boolean pMina) {
+		this.mina = pMina;
+	}
+
+}
--- ItsasGudua/src/org/minakdev/itsasgudua/Tableroa.java
+++ ItsasGudua/src/org/minakdev/itsasgudua/Tableroa.java
@@ -1,5 +1,147 @@
 package org.minakdev.itsasgudua;
 
 public class Tableroa {
-
+	
+	private Laukia[][] lista;
+	private static int minaKop = 3;
+	private static int tamaina = 6;
+	
+	public Tableroa() {
+		this.lista = new Laukia[tamaina][tamaina];
+	}
+	
+	public void itsasontziaJarri(int pX, int pY, int pTamaina, char pNoranzkoa, Itsasontzia pItsasontzia) {
+		if(pNoranzkoa == 'B') {
+			for(int i = 1; i < pTamaina; i++) {
+				this.lista[pX+i][pY].setItsasontzia(pItsasontzia);
+			}
+		}
+		else {
+			for(int i = 0; i < pTamaina; i++) {
+				this.lista[pX][pY+1].setItsasontzia(pItsasontzia);			
+			}
+		}
+	}
+	
+	public void minakJarri() {
+		for(int i = 0; i < minaKop; i++ ) {
+			boolean topatua = false;
+			while(!topatua) {
+				int x = (int)(Math.random()*(tamaina+1));
+				int y = (int)(Math.random()*(tamaina+1));
+				if(this.koordenatuEgokiak(x, y)) {
+					this.lista[x][y].setMina(true);
+					topatua = true;
+				}
+			}
+			
+		}
+	}
+	
+	public Itsasontzia itsasontzirikDago(int pX, int pY) {
+		return this.lista[pX][pY].getItsasontzia();
+	}
+	
+	public boolean minarikDago(int pX, int pY) {
+		return this.lista[pX][pY].getMina();
+	}
+	
+	public void tableroOsoaInprimatu() {
+		for(int i = 0; i <= tamaina; i++) {
+			for(int j = 0; j <= tamaina; j++) {
+				this.lista[i][j].nireaInprimatu();
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public void minakBoom(int pX, int pY, Jokalaria pAurkaria) {
+		//1
+		if(this.koordenatuEgokiak(pX - 1, pY - 1)){
+			if(!this.begiratutaDago(pX - 1, pY - 1)) {
+				this.minaLehertu(pX - 1, pY - 1, pAurkaria);
+			}
+		}
+		//2
+		else if(this.koordenatuEgokiak(pX, pY - 1)){
+			if(!this.begiratutaDago(pX, pY - 1)) {
+				this.minaLehertu(pX, pY - 1, pAurkaria);
+			}
+		}
+		//3
+		else if(this.koordenatuEgokiak(pX + 1, pY - 1)){
+			if(!this.begiratutaDago(pX + 1, pY - 1)) {
+				this.minaLehertu(pX + 1, pY - 1, pAurkaria);
+			}
+		}
+		//4
+		else if(this.koordenatuEgokiak(pX - 1, pY)){
+			if(!this.begiratutaDago(pX - 1, pY)) {
+				this.minaLehertu(pX - 1, pY, pAurkaria);
+			}
+		}
+		//6
+		else if(this.koordenatuEgokiak(pX + 1, pY)){
+			if(!this.begiratutaDago(pX + 1, pY)) {
+				this.minaLehertu(pX + 1, pY, pAurkaria);
+			}
+		}
+		//7
+		else if(this.koordenatuEgokiak(pX - 1, pY + 1)){
+			if(!this.begiratutaDago(pX - 1, pY + 1)) {
+				this.minaLehertu(pX - 1, pY + 1, pAurkaria);
+			}
+		}
+		//8
+		else if(this.koordenatuEgokiak(pX, pY + 1)){
+			if(!this.begiratutaDago(pX, pY + 1)) {
+				this.minaLehertu(pX, pY + 1, pAurkaria);
+			}
+		}
+		//9
+		else if(this.koordenatuEgokiak(pX + 1, pY + 1)){
+			if(!this.begiratutaDago(pX + 1, pY + 1)) {
+				this.minaLehertu(pX + 1, pY + 1, pAurkaria);
+			}
+		}
+	}
+	
+	public void egungoTableroaInprimatu() {
+		for(int i = 0; i <= tamaina; i++) {
+			for(int j = 0; j <= tamaina; j++) {
+				this.lista[i][j].aurkariarenaInprimatu();
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public boolean begiratutaDago(int pX, int pY) {
+		return this.lista[pX][pY].getBegiratuta();
+	}
+	
+	public boolean koordenatuEgokiak(int pX, int pY) {
+		if(pX < 0 || pX > tamaina || pY < 0 || pY > tamaina ) {
+			return false;
+		}
+		else {
+			if(this.lista[pX][pY].getMina() || this.lista[pX][pY].getItsasontzia() != null) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+	}
+	
+	private void minaLehertu(int pX, int pY, Jokalaria pAurkaria) {
+		if(this.itsasontzirikDago(pX, pY) != null){
+			if()
+		}
+	}
+	
+	public void setBegiratuta(int pX, int pY, boolean pBegiratuta) {
+		this.lista[pX][pY].setBegiratuta(pBegiratuta);
+	}
 }
