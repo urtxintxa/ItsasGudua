1b2d84883ca2977c02d5ff2675a0f2402f4ad6f6
amaia <@Amaia>
2015-04-20 22:57:49 +0200

    Metodo batzuk gehituta

--- ItsasGudua/src/org/minakdev/itsasgudua/Jokoa.java
+++ ItsasGudua/src/org/minakdev/itsasgudua/Jokoa.java
@@ -11,7 +11,8 @@ public class Jokoa {
 	private Scanner sc = new Scanner(System.in);
 	
 	private Jokoa() {
-		
+		this.jokalariak= new Jokalaria[2];
+    	this.txanda=0;
 	}
 	
 	public Jokoa getNireJokoa() {
@@ -22,16 +23,64 @@ public class Jokoa {
 	}
 	
 	public void partidaBatjolastu() {
+		Jokalaria irabazlea=null;
+    	
+    	
+    	System.out.println("Lehengo jokalaria, sartu izena.");
+    	this.jokalariak[0]= new Jokalaria(this.izenaEskatu());
+    	System.out.println("Bigarren jokalaria, sartu izena.");
+    	this.jokalariak[1]= new Jokalaria(this.izenaEskatu());
+    	
+    	System.out.println(this.jokalariak[0].getIzena() + ", zure tableroa prestatu.");
+    	this.jokalariak[0].tableroaPrestatu();
+    	System.out.println(this.jokalariak[1].getIzena() + ", zure tableroa prestatu.");
+    	this.jokalariak[1].tableroaPrestatu();
+    	
+    	System.out.println("Has dadila partida.");
+    	
+    	while(irabazlea==null){
+    		if(!this.jokalariak[this.txanda % 2].getPenalizazioa()){
+    			this.jokalariak[this.txanda % 2].tiroEgin(this.jokalariak[(this.txanda + 1) % 2]);
+    		}
+    		this.txanda++;
+    		irabazlea=this.partidarenIrabazlea();
+    	}
+    	
+    	
+    	System.out.println("Jokoa amaitu egin da.");
+    	System.out.printf("Jokoaren irabazlea %s da", irabazlea.getIzena());
+    	System.out.println();
+    	
+    	this.jokalarienEgoeraInprimatu();
+    	
+    	this.jokalariak[0].guztizInprimatu();
+    	this.jokalariak[1].guztizInprimatu();
+
 		
 	}
 	
 	private String izenaEskatu() {
-		System.out.println("Sartu zure izena: ");
+		//System.out.println("Sartu zure izena: ");
 		String izena = sc.next();
 		return izena;
 	}
 
 	public Jokalaria partidarenIrabazlea() {
-		
+		Jokalaria irabazlea=null;
+        if(this.jokalariak[0].minaMaxGainditua()||this.jokalariak[0].zenbatItsasontzi()==0) {
+        	irabazlea=this.jokalariak[1];
+        }  
+        if(this.jokalariak[1].minaMaxGainditua()||this.jokalariak[1].zenbatItsasontzi()==0) {
+        	irabazlea=this.jokalariak[0];
+        } 	
+        
+        return irabazlea;
 	}
+	
+	private void jokalarienEgoeraInprimatu(){
+    	
+    	this.jokalariak[0].jokalariarenEgoeraInprimatu();
+    	this.jokalariak[1].jokalariarenEgoeraInprimatu();
+    	
+    }
 }
