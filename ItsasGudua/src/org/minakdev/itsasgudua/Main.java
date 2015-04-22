e046cf66fdc54562a4665be717ecf5a508475a3a
jajasuperman <enekosampedro@gmail.com>
2015-04-22 19:17:26 +0200

    Main metodoa eraikita (gauza asko falta zaizkio)

---
+++ ItsasGudua/src/org/minakdev/itsasgudua/Main.java
@@ -0,0 +1,31 @@
+package org.minakdev.itsasgudua;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Jokoa gureJokoa = Jokoa.getNireJokoa();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Ongi etorri Itsas Gudua jokora :D");
+		System.out.println("Jolasteko 1 zenbakia sakatu.");
+		System.out.println("Irteteko 0 zenbakia sakatu.");
+
+		int aukera = sc.nextInt();
+		
+		//Exception-a falta da.
+		
+		if(aukera == 1) {
+			gureJokoa.partidaBatjolastu();
+		}
+		else {
+			System.exit(0);
+		}
+		
+
+	}
+
+}
