20c6b8f4d818e1073ed81875d23a11763bcc8bd1
jajasuperman <enekosampedro@gmail.com>
2015-05-06 10:49:39 +0200

    o

--- ItsasGudua/src/org/minakdev/itsasgudua/Tableroa.java
+++ ItsasGudua/src/org/minakdev/itsasgudua/Tableroa.java
@@ -4,7 +4,7 @@ public class Tableroa {
 	
 	private Laukia[][] lista;
 	private static int minaKop = 6;
-	private static int tamaina = 4;
+	private static int tamaina = 8;
 	
 	public Tableroa() {
 		this.lista = new Laukia[tamaina][tamaina];
