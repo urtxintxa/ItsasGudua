81dd90fa6c3edb4c4dbb95c9672ee8c2bbf5c303
amaia <amitxu96@gmail.com>
2015-05-06 21:21:56 +0200

    Koordenatuak kalkulatzea "hobetua"

--- ItsasGudua/src/org/minakdev/itsasgudua/Ordenagailua.java
+++ ItsasGudua/src/org/minakdev/itsasgudua/Ordenagailua.java
@@ -29,16 +29,13 @@ public class Ordenagailua extends Jokalaria{
 	    	else{
 	    		this.jokalariarenEgoeraInprimatu();
 	    		pAurkari.jokalariarenEgoeraInprimatu();
-	    		this.aurkariarenTableroaInprimatu(pAurkari);
-	    		
-	    		
-		    	
-			    koor=this.kalkulatuHurrengoKoordenatuak();
+	    		pAurkari.partzialkiInprimatu();
+	    			    	
+			    koor=this.kalkulatuHurrengoKoordenatuak(pAurkari);
 			    x= koor.getX();
 		    	y= koor.getY();		
 
-
-		    	
+	    	
 		    	aurkariarenTableroa.setBegiratuta(x, y, true);
 		    	Itsasontzia its=aurkariarenTableroa.itsasontzirikDago(x, y);
 		    	if(its!=null){
@@ -82,133 +79,182 @@ public class Ordenagailua extends Jokalaria{
 		    				aurkariarenTableroa.minakBoom(x, y, pAurkari);
 		    			}
 		    		}
-		    	}
-		    	
-		    	
-		    	
-	    		
+		    	}	    		
 	    		
 	    	}
 	    	
 	    	
 	    }
 
-	    public void itsasontziaJarri(Itsasontzia pItsasontzia){
-	    	int x=this.lortuX();
-	    	int y=this.lortuY();
-	    	char horBer=this.lortuHorBer();
-	    	int tamaina= pItsasontzia.getTamainaMax();
-	    	boolean koordenatuZuzenak = this.koordenatuZuzenak(x, y, horBer, tamaina);
-	    	
-	    	while(!koordenatuZuzenak){
-	    		x=this.lortuX();
-	        	y=this.lortuY();
-	        	horBer=this.lortuHorBer();
-	        	koordenatuZuzenak = this.koordenatuZuzenak(x, y, horBer, tamaina);
-	    	}
-	    	
-	    	this.getListaItsasontziak().gehituItsasontzia(pItsasontzia);
-	    	this.getTableroa().itsasontziaJarri(x, y, tamaina, horBer, pItsasontzia);
-	    	
-	    }
 
-	    private int lortuX(){
-	    	int x = (int)Math.random()*(Tableroa.getTamaina()  + 1);
-	    	return x;
-	    }
-
-	    private int lortuY(){
-	    	int x = (int)Math.random()*(Tableroa.getTamaina()  + 1);
-	    	return x;
-	    }
-
-	    private char lortuHorBer(){
-	        
-	    }
-
-	    private Koordenatuak kalkulatuHurrengoKoordenatuak(Jokalaria pAurkari){//parametrorik gabe
+	    private Koordenatuak kalkulatuHurrengoKoordenatuak(Jokalaria pAurkari){
 	    	int x;
 	    	int y;
 	    	Koordenatuak koor;
+			Koordenatuak k1;
+			Koordenatuak k2;
 	    	boolean koordenatuEgokiak=false;
 	    	int zenbatKoor=this.zenbatKoordenatu();
 	    	boolean jarraitu=true;
-	    	
+	    	Tableroa aurkariarenTableroa=pAurkari.getTableroa();
 	    	
 	    	if(zenbatKoor==0){
 	    		while(!koordenatuEgokiak){
-		    		x=this.lortuX();
-		    		y= this.lortuY();
-		    		koordenatuEgokiak=this.getTableroa().koordenatuEgokiak(x, y);   
+		    		x = this.lortuXY();
+		    		y = this.lortuXY();
+		    		koordenatuEgokiak=aurkariarenTableroa.koordenatuEgokiak(x, y);   
 		    		if(koordenatuEgokiak){
-		    			koordenatuEgokiak=!pAurkari.getTableroa().begiratutaDago(x, y);
-		    			//
-		    			
-		    			
-		    			
-		    			
+		    			koordenatuEgokiak=!aurkariarenTableroa.begiratutaDago(x, y);
+						koor = new Koordenatuak(x,y);
 		    		}
 	    		}
 	    	}
 	    	else{
 	    		if(zenbatKoor==1){
-	    			koor=this.itsasontzienKoordenatuak.get(0);
-	    			x=koor.getX();
-	    			y=koor.getY(); 
-		    		if(this.getTableroa().koordenatuEgokiak(x-1, y)){
-		    			if(!pAurkari.getTableroa().begiratutaDago(x-1, y)){
+	    			k1=this.itsasontzienKoordenatuak.get(0);
+	    			x=k1.getX();
+	    			y=k1.getY(); 
+		    		if(aurkariarenTableroa.koordenatuEgokiak(x-1, y)){
+		    			if(!aurkariarenTableroa.begiratutaDago(x-1, y)){
 		    				return new Koordenatuak(x-1, y);
 		    			}
 		    		}
-		    		if(this.getTableroa().koordenatuEgokiak(x, y+1)){
-		    			if(!pAurkari.getTableroa().begiratutaDago(x, y+1)){
+		    		if(aurkariarenTableroa.koordenatuEgokiak(x, y+1)){
+		    			if(!aurkariarenTableroa.begiratutaDago(x, y+1)){
 		    				return new Koordenatuak(x, y+1);
 		    			}
 		    		}
-		    		if(this.getTableroa().koordenatuEgokiak(x+1, y)){
-		    			if(!pAurkari.getTableroa().begiratutaDago(x+1, y)){
+		    		if(aurkariarenTableroa.koordenatuEgokiak(x+1, y)){
+		    			if(!aurkariarenTableroa.begiratutaDago(x+1, y)){
 		    				return new Koordenatuak(x+1, y);
 		    			}
 		    		}
-		    		if(this.getTableroa().koordenatuEgokiak(x, y-1)){
-		    			if(!pAurkari.getTableroa().begiratutaDago(x, y-1)){
+		    		if(aurkariarenTableroa.koordenatuEgokiak(x, y-1)){
+		    			if(!aurkariarenTableroa.begiratutaDago(x, y-1)){
 		    				return new Koordenatuak(x, y-1);
 		    			}
 		    		}
 		    		this.itsasontzienKoordenatuak.remove(0);
-	    			this.kalkulatuHurrengoKoordenatuak(pAurkari);
+						return this.kalkulatuHurrengoKoordenatuak(pAurkari);
 	    		}
 	    		else{
-	    			koor=this.itsasontzienKoordenatuak.get(0);
-	    			Koordenatuak koorAux=this.itsasontzienKoordenatuak.get(1);
-	    			x=koor.getX()-koorAux.getX();
-	    			y=koor.getY()-koorAux.getY();
-	    			if(y==0){
-	    				x=koor.getX()+1;
-	    				y=koor.getY();
+	    			k1=this.itsasontzienKoordenatuak.get(0);
+	    			k2=this.itsasontzienKoordenatuak.get(1);
+	    			x=k1.getX()-k2.getX();
+	    			y=k1.getY()-k2.getY();
+	    			boolean noranzkoaAldatu=false;
+					if(y==0){
+	    				x=k1.getX()+1;
+	    				y=k1.getY();
 	    				while(jarraitu){
-	    					if(this.getTableroa().koordenatuEgokiak(x, y)){
-	    						if(!pAurkari.getTableroa().begiratutaDago(x, y)){
-	    							return 
+	    					if(aurkariarenTableroa.koordenatuEgokiak(x, y)){
+	    						if(!aurkariarenTableroa.begiratutaDago(x, y)){
+	    							koor= new Koordenatuak(x,y);
+									jarraitu=false;
 	    						}
+								else{
+									if(aurkariarenTableroa.itsasontzirikDago(x,y)==null){
+										noranzkoaAldatu=true;
+										jarraitu=false;
+									}
+									else{x++;}
+								}
 	    					}
+							else{
+								noranzkoaAldatu=true;
+								jarraitu=false;
+							}
+							
 	    				}
+						if(noranzkoaAldatu){
+							jarraitu=true;
+							x=k1.getX()-1;
+							y=k1.getY();
+							while(jarraitu){
+								if(aurkariarenTableroa.koordenatuEgokiak(x, y)){
+									if(!aurkariarenTableroa.begiratutaDago(x, y)){
+										koor= new Koordenatuak(x,y);
+										jarraitu=false;
+									}
+									else{
+										if(aurkariarenTableroa.itsasontzirikDago(x,y)==null){
+											jarraitu=false;
+										}
+										else{x--;}
+									}
+								}
+								else{
+									jarraitu=false;
+								}
+							
+							}
+						}
 	    			}
+					else{
+						if(x==0){
+							x=k1.getX();
+							y=k1.getY()+1;
+							while(jarraitu){
+								if(aurkariarenTableroa.koordenatuEgokiak(x, y)){
+									if(!aurkariarenTableroa.begiratutaDago(x, y)){
+										koor= new Koordenatuak(x,y);
+										jarraitu=false;
+									}
+									else{
+										if(aurkariarenTableroa.itsasontzirikDago(x,y)==null){
+											noranzkoaAldatu=true;
+											jarraitu=false;
+										}
+										else{y++;}
+									}
+								}
+								else{
+									noranzkoaAldatu=true;
+									jarraitu=false;
+								}
+								
+							}
+							if(noranzkoaAldatu){
+								jarraitu=true;
+								x=k1.getX();
+								y=k1.getY()-1;
+								while(jarraitu){
+									if(aurkariarenTableroa.koordenatuEgokiak(x, y)){
+										if(!aurkariarenTableroa.begiratutaDago(x, y)){
+											koor= new Koordenatuak(x,y);
+											jarraitu=false;
+										}
+										else{
+											if(aurkariarenTableroa.itsasontzirikDago(x,y)==null){
+												jarraitu=false;
+											}
+											else{y--;}
+										}
+									}
+									else{
+										jarraitu=false;
+									}
+								
+								}
+							}
+						}
+						
+					}
 	    		}
 	    		
 	    	}
+			if(koor==null){
+				//zelan??????
+			}
 	    	
 	    	
-	    	return
+	    	return koor;
 	    }
 
 	    private void gehituKoordenatuak(Koordenatuak pKoordenatuak){
 	    	this.itsasontzienKoordenatuak.add(pKoordenatuak);
 	    }
 
-	    private void kenduKoordenatuak(Koordenatuak pKoordenatuak){
-	    	this.itsasontzienKoordenatuak.remove(pKoordenatuak);
-	    }
 	    
 	    private int zenbatKoordenatu(){
 	    	return this.itsasontzienKoordenatuak.size();
