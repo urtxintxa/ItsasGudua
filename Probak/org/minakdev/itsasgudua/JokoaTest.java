34012bc50cf128386a8efe5c5bd01140f2cacb89
jajasuperman <enekosampedro@gmail.com>
2015-05-12 22:20:05 +0200

    JokoaTest

--- Probak/org/minakdev/itsasgudua/JokoaTest.java
+++ Probak/org/minakdev/itsasgudua/JokoaTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 public class JokoaTest {
 	
 	Jokoa gureJokoa = Jokoa.getNireJokoa();
+	
 
 	@Before
 	public void setUp() throws Exception {
@@ -22,16 +23,35 @@ public class JokoaTest {
 	@Test
 	public void testMain() {
 		String[] a = null;
-		Jokoa.main(a);
 		
 		/*
-		Kontsolako irteeran begiratuko dugu ea eskatuIzena
-		metodoak izena hartzen duen. Hala ere, exception-ekin
-		tratatua denez, %100an badakigu izen oso bat sartuko dela
+		Main metodoak, jokatzekoModuaEskatu metodoa deitzen du.
+		Metodo honek, zenbaki bat eskatuko du, 1, 2 edo 3.
 		
-		jokatzekoModuaEskatu eta jokatzekoParametroakEskatu metodoek Exception-ak
-		ere erabiltzen dituzte, beraz zihurtatu dezakegu funtzionatuko dutela
+			1. proba zenbaki oso bat ez den karakterea sartzen probatuko dugu
+			gureJokoa.main(a);
 		
+			2. proba tarte horretatik ez dagoen zenbaki bat sartuz
+			gureJokoa.main(a);
+		
+			3. proba, 1 zenbakia sartuz (partidaBatJolastu() deituko du)
+			gureJokoa.main(a);
+		
+			4. proba 2 zenbakia sartuz (partidaBatJolastu1() deituko du)
+			gureJokoa.main(a);
+			
+			5. proba 3 zenbakia sartuz (partidaBatJolastu2() deituko du)
+			gureJokoa.main(a);
+		
+		partidaBatJolastu metodoek, jokatzekoParametroakEskatu() metodoa deituko dute, tableroaren tamaina ezartzeko
+		Hemen ere, beste kasu batzuk agertzen zaizkigu, beraz guztiak probatu behar
+			1. zenbaki oso bat ez sartzea
+			2. zenbakia tartetik kanpo egotea
+			3. 1 zenbakia sartzea
+			4. 2 zenbakia sartzea
+			5. 3 zenbakia sartzea
+		
+		Proba guztietan izen desberdinak sartzen probatu dugu, eta guztietan funtzionatu du.
 		
 		*/
 		
@@ -54,10 +74,5 @@ public class JokoaTest {
 		gureJokoa.soinuaErreproduzitu(Jokoa.itsasontziaJo); //soinuak karpetan dagoen soinua
 		
 		gureJokoa.soinuaErreproduzitu("kaixo"); //Exception bat
-	}
-
-	@Test
-	public void testNoriTokatzenZaio() {
-		assertSame(gureJokoa.noriTokatzenZaio(), null);
 	}
 }
